3e435df003e304a575b7bb276168aa3f75170a00
Dmitrii <karpinov92@gmail.com>
2023-03-11 13:04:13 +0300

    Начали изучать ООП. Начали сощдавать классы по диаграмме. Д/З - необходимо доделать оставшиеся классы из диаграммы, прописать им поля и функции

---
+++ lesson_11.03.2023_3(разработка программных модулей).py
@@ -0,0 +1,37 @@
+class Person:
+    def __init__(self, n: str, pn: str = '', ea: str = '',): #, a
+        self.name = n
+        self.phnum = pn
+        self.eadr = ea
+        self.adr = Address()  # - композиция
+        # self.adr = a - агрегация
+
+    def pppass(self, ):
+        pass
+
+
+class Address:
+    pass
+
+
+class Student(Person):
+    pass
+
+
+class Professor(Person):
+    pass
+
+
+def main():
+    print('Вызов конструктора')
+    p = Person('Tom')  # На самом деле вызываются __new__ __init__
+    print(p.name)
+
+
+if __name__ == '__main__':
+    main()
+
+'''
+Почитать:
+https://metanit.com/python/tutorial/7.1.php
+'''
