aa67ec1d4fd3479a103fc54ec1b5cc1c41e10be9
Dmitrii <karpinov92@gmail.com>
2023-03-12 19:54:45 +0300

    Решил задание Питоньтютор 11_2. В PeCharm все тесты проходятся. На сайте питонтьютора почему-то не выводится ответ (не выводится ничего).

---
+++ Домашняя работа/Питонтьютор/task_11_2.py
@@ -0,0 +1,17 @@
+'''
+Вам дан словарь, состоящий из пар слов. Каждое слово является синонимом к парному ему слову.
+Все слова в словаре различны.
+
+Для слова из словаря, записанного в последней строке, определите его синоним.
+'''
+words = {}
+x= int(input())
+for i in range(x):
+    words.update([input().split()])
+value = input()
+for key in words:
+    if words[key] == value:
+        print(key)
+    elif key == value:
+        print(words[key])
+
