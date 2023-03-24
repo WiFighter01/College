1036712cf5141b6f4af250114cc96c8b12de094d
Dmitrii <karpinov92@gmail.com>
2023-03-24 20:05:35 +0300

    Добавил функцию минус, умножение на число и определитель 2 порядка

--- Домашняя работа/Задача на двумерные массивы.py
+++ Домашняя работа/Задача на двумерные массивы.py
@@ -1,13 +1,12 @@
 from random import randint
 
 
-def matrix(n: int, m: int, a: int = 0, b: int = 1):
+def matrix(n: int, m: int, a: int = 1, b: int = 3):
     '''
     Генерирует матрицу размера n (кол-во строк) на m (кол-во столбцов),
     заполненную случайными числами в диапазоне от a до b
     '''
     return [[randint(a, b) for i in range(m)] for j in range(n)]
-
 
 def print_m(a):
     '''
@@ -15,7 +14,6 @@ def print_m(a):
     '''
     for str_m in a:
         print(*str_m)
-
 
 def sum_m(a, b):
     '''
@@ -28,6 +26,16 @@ def sum_m(a, b):
             res[i].append(a[i][j] + b[i][j])
     return res
 
+def minus_m(a, b):
+    '''
+    Вычитаем матрицы a и b
+    '''
+    res = []
+    for i in range(len(a)):
+        res.append([])
+        for j in range(len(a[i])):
+            res[i].append(a[i][j] - b[i][j])
+    return res
 
 def mult_m(a, b):
     '''
@@ -43,11 +51,54 @@ def mult_m(a, b):
                 res[i][j] += a[i][x] * b[x][j]
     return res
 
+def mult_m_2(a, b: int):
+    '''
+    Перемножаем матрицу a на число b
+    '''
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            a[i][j] *= b
+    return a
 
+def opred_2(a):
+    '''
+    Вычисляем определитель второго порядка для квадратной матрицы a
+    '''
+    res = a[0][0]*a[1][1] - a[0][1]*a[1][0]
+    return res
+
+
+# умножение матриц
 a1 = matrix(3, 2)
-a2 = matrix(2, 5)
+a2 = matrix(2, 3)
 print_m(a1)
 print('*')
 print_m(a2)
 print('=')
 print_m(mult_m(a1, a2))
+print()
+
+# Вычитание матриц
+b1 = matrix(3, 3)
+b2 = matrix(3, 3)
+print_m(b1)
+print('-')
+print_m(b2)
+print('=')
+print_m(minus_m(b1, b2))
+print()
+
+# Умножение матрицы на число
+c1 = matrix(4, 2)
+c2 = 5
+print_m(c1)
+print('*')
+print(c2)
+print('=')
+print_m(mult_m_2(c1, c2))
+print()
+
+# Определитель 2 порядка
+d1 = matrix(2, 2)
+print_m(d1)
+print('определитель матрицы равен =', opred_2(d1))
