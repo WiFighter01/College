cc4d82a7f51982cf0d078270e0cc09fa0fa31ced
Dmitrii <karpinov92@gmail.com>
2023-03-27 16:04:51 +0300

    Добавил setUp в тесты по матрицам

--- Homework/test_matrix.py
+++ Homework/test_matrix.py
@@ -2,38 +2,39 @@ import unittest
 import matrix
 
 
-class MatrixTest(unittest.TestCase):
+class TestMatrixFunctions(unittest.TestCase):
+    def setUp(self):
+        self.a = [[0, 1], [1, 1]]
+        self.b = [[2, 1], [1, 3]]
+        self.c = [[2, 2], [2, 4]]
+        self.d = [[-2, 0], [0, -2]]
+        self.e = [[1, 3], [3, 4]]
+        self.g = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
     def test_matrix(self):
         self.assertEqual(matrix.matrix(2, 2, 1, 1), [[1, 1], [1, 1]])
 
     def test_sum_m(self):
-        self.assertEqual(matrix.sum_m(a, b), c)
+        self.assertEqual(matrix.sum_m(self.a, self.b), self.c)
 
     def test_minus_m(self):
-        self.assertEqual(matrix.minus_m(a, b), d)
+        self.assertEqual(matrix.minus_m(self.a, self.b), self.d)
 
     def test_mult_m(self):
-        self.assertEqual(matrix.mult_m(a, b), e)
+        self.assertEqual(matrix.mult_m(self.a, self.b), self.e)
 
     def test_mult_m_2(self):
         self.assertEqual(matrix.mult_m_2([[1, 1], [1, 1]], 5), [[5, 5], [5, 5]])
 
     def test_opred_2(self):
-        self.assertEqual(matrix.opred_2(a), -1)
+        self.assertEqual(matrix.opred_2(self.a), -1)
 
     def test_opred_3(self):
-        self.assertEqual(matrix.opred_3(g), 0)
+        self.assertEqual(matrix.opred_3(self.g), 0)
 
     def test_matrix_equal(self):
-        self.assertEqual(matrix.matrix_equal(a, b), True)
+        self.assertEqual(matrix.matrix_equal(self.a, self.b), True)
 
-
-a = [[0, 1], [1, 1]]
-b = [[2, 1], [1, 3]]
-c = [[2, 2], [2, 4]]
-d = [[-2, 0], [0, -2]]
-e = [[1, 3], [3, 4]]
-g = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 if __name__ == '__main__':
     unittest.main()
