7c0c1cfc5e79b4b7fd78d5417f8af4d70643212f
Your Name <your_email@whatever.com>
2023-06-17 11:34:23 +0300

    Доработал алгоритмы поска по тексту

--- Homework/text_search_algo.py
+++ Homework/text_search_algo.py
@@ -1,4 +1,46 @@
-# Метод КМП
+# 1. Алгоритмы поиска текстовой информации (наивный)
+def test(text, pattern):
+    if not pattern:
+        print('Шаблон пустой')
+        return False
+    if not text:
+        print('Текст пустой')
+        return False
+    if len(pattern) > len(text):
+        print('Текст короче шаблона')
+        return False
+    return True
+
+
+# O(n*m)
+def find(text, pattern):
+    n = len(text)
+    m = len(pattern)
+    i = 0
+    finded = False
+    while i <= n - m:
+        for j in range(m):
+            if text[i + j] != pattern[j]:
+                break
+        else:
+            finded = True
+            print('Шаблон найден по индексу ', i)
+            # i = n для поиска только первого вхождения
+        i += 1
+    if not finded:
+        print('Шаблон не найден')
+        # T = AAAAAAAAAA, P = AAAB
+
+print('1. Наивный поиск')
+text = 'ABCABAABCABAC'
+pattern = 'CAB'
+if test(text, pattern):
+    find(text, pattern)
+print()
+
+
+# 2. Метод КМП
+print('2. КМП')
 t = "лилила"
 
 p = [0] * len(t)
